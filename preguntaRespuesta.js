4b33e9818ad81c011bca5cec1e83661f78cdc44c
LuisaEstradaY <yepesluu@gmail.com>
2024-01-22 18:45:23 -0500

    new id

--- preguntaRespuesta.js
+++ preguntaRespuesta.js
@@ -14,7 +14,7 @@ const openai = new OpenAI({
 
 app.post('/get-response', async (req, res) => {
   try {
-    const modelId = "ft:gpt-3.5-turbo-1106:equilibria::8iPnEt3k"; // Reemplaza con el ID de tu modelo Fine-tuned
+    const modelId = "ft:gpt-3.5-turbo-1106:equilibria::8juqiVbn"; // Reemplaza con el ID de tu modelo Fine-tuned
     const question = req.body.question;
 
     if (typeof question !== 'string' || question.trim().length === 0) {

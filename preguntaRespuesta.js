a3a14c209ce2d6c768365d895630006225e6a426
LuisaEstradaY <yepesluu@gmail.com>
2024-02-05 22:34:34 -0500

    update, de id

--- preguntaRespuesta.js
+++ preguntaRespuesta.js
@@ -14,7 +14,7 @@ const openai = new OpenAI({
 
 app.post('/get-response', async (req, res) => {
   try {
-    const modelId = "ft:gpt-3.5-turbo-1106:equilibria::8p58weFB";// Reemplaza con el ID de tu modelo Fine-tuned
+    const modelId = "ft:gpt-3.5-turbo-1106:equilibria::8p6IHucG";// Reemplaza con el ID de tu modelo Fine-tuned
     const question = req.body.question;
 
     if (typeof question !== 'string' || question.trim().length === 0) {
@@ -24,8 +24,8 @@ app.post('/get-response', async (req, res) => {
     const chatCompletion = await openai.chat.completions.create({
       messages: [{
         "role": "system",
-        "content": "No vas a responder preguntas que no sean relacionadas al limon NUNCA"
-      }, { role: 'user', content: question }],
+        "content": "Eres Germán, el asistente agrónomo de Equilibria, tu enfoque y fuerte es el Limón Tahíti, solo respondes preguntas relacionadas al limon, cultivacion, cuidados, consejos, tips y dudas relacionadas con estos temas, la idea es que apoyes siempre solo estos temas y seas formal, tu solo respondes preguntas de limon, cultivacion, cuidados, consejos, tips y dudas relacionadas con estos temas, si te preguntan por temas distintos como deportes, historia, matematicas o mas le dices que tu conocimiento se limita a cítricos en general y especialmente Limón Tahití. Responderas amablamente siempre saludando, palabras cortas sin sentido y cuando hablen de otros temas los orientas a que pregunten por estos temas que te mencione. Que las respuestas sean largas no cortas preferiblemente de mas de 150 palabras. NUNCA RESPONDER PREGUNTAS NO RELACIONADAS CON LIMON, CULTIVACION, TIPS, CUIDADOS, CONSEJOS Y/O CONSULTAS QUE NO SEAN DE LIMON. Responde las preguntas y respuestas que esten precargadas"
+      }, { role: 'user', content: question }], 
       model: modelId,
     });
 

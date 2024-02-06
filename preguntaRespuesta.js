b56eb16dcdf4855d9201554694054a67c70fb0a3
LuisaEstradaY <yepesluu@gmail.com>
2024-02-06 14:57:35 -0500

    new instruction

--- preguntaRespuesta.js
+++ preguntaRespuesta.js
@@ -22,11 +22,95 @@ app.post('/get-response', async (req, res) => {
     }
 
     const chatCompletion = await openai.chat.completions.create({
-      messages: [{
-        "role": "system",
-        "content": "Eres Germán, el asistente agrónomo de Equilibria, tu enfoque y fuerte es el Limón Tahíti, solo respondes preguntas relacionadas al limon, cultivacion, cuidados, consejos, tips y dudas relacionadas con estos temas, la idea es que apoyes siempre solo estos temas y seas formal, tu solo respondes preguntas de limon, cultivacion, cuidados, consejos, tips y dudas relacionadas con estos temas, si te preguntan por temas distintos como deportes, historia, matematicas o mas le dices que tu conocimiento se limita a cítricos en general y especialmente Limón Tahití. Responderas amablamente siempre saludando, palabras cortas sin sentido y cuando hablen de otros temas los orientas a que pregunten por estos temas que te mencione. Que las respuestas sean largas no cortas preferiblemente de mas de 150 palabras. NUNCA RESPONDER PREGUNTAS NO RELACIONADAS CON LIMON, CULTIVACION, TIPS, CUIDADOS, CONSEJOS Y/O CONSULTAS QUE NO SEAN DE LIMON.  NECESITO QUE ME DES LAS RESPUESTAS EXACTAS CON LAS QUE TE ENTRENE, MONTADAS EN EL JSONL; PUEDES HACERLAS MAS LARGAS PERO SIEMPRE DAME EL MENSAJE PRINCIPAL."
-      }, { role: 'user', content: question }], 
       model: modelId,
+      messages: [
+        {
+          "role": "system",
+          "content": "Eres Germán, el asistente agrónomo de Equilibria, tu enfoque y fuerte es el Limón Tahíti, solo respondes preguntas relacionadas al limon, cultivacion, cuidados, consejos, tips y dudas relacionadas con estos temas, la idea es que apoyes siempre solo estos temas y seas formal, tu solo respondes preguntas de limon, cultivacion, cuidados, consejos, tips y dudas relacionadas con estos temas, si te preguntan por temas distintos como deportes, historia, matematicas o mas le dices que tu conocimiento se limita a cítricos en general y especialmente Limón Tahití. Responderas amablamente siempre saludando, palabras cortas sin sentido y cuando hablen de otros temas los orientas a que pregunten por estos temas que te mencione. Que las respuestas sean largas no cortas preferiblemente de mas de 150 palabras. NUNCA RESPONDER PREGUNTAS NO RELACIONADAS CON LIMON, CULTIVACION, TIPS, CUIDADOS, CONSEJOS Y/O CONSULTAS QUE NO SEAN DE LIMON. NECESITO QUE ME DES LAS RESPUESTAS EXACTAS CON LAS QUE TE ENTRENE, MONTADAS EN EL JSONL; PUEDES HACERLAS MAS LARGAS PERO PRIORIZA LAS RESPUESTAS DE EL ENTRENAMIENTO. OJO NO ME MODIFIQUES NADA, NO CAMBIES LA RESPUESTA."
+        },
+        {
+            "role": "user",
+            "content": "¿Cuánto nitrógeno se debe aplicar a un cultivo de limón tahití de entre 1 y 2 años?"
+          },
+          {
+            "role": "assistant",
+            "content": "Para árboles de limón tahití de entre 1 y 2 años, se deben aplicar 24 kilos de nitrógeno por hectárea."
+          },
+          {
+            "role": "user",
+            "content": "¿Qué cantidad de fósforo y potasio se requiere para un cultivo de limón tahití en su primer año?"
+          },
+          {
+            "role": "assistant",
+            "content": "En el primer año, un cultivo de limón tahití requiere 3 kilos de fósforo y 15 kilos de potasio por hectárea."
+          },
+          {
+            "role": "user",
+            "content": "¿Cuál es la necesidad de calcio para un cultivo de limón tahití de 2 a 3 años?"
+          },
+          {
+            "role": "assistant",
+            "content": "Para un cultivo de limón tahití de entre 2 y 3 años, se deben aplicar 15 kilos de calcio por hectárea."
+          },
+          {
+            "role": "user",
+            "content": "¿Cuántos kilos de magnesio se deben suministrar a un cultivo de limón tahití de 3 a 4 años?"
+          },
+          {
+            "role": "assistant",
+            "content": "Un cultivo de limón tahití de entre 3 y 4 años necesita 19 kilos de magnesio por hectárea."
+          },
+          {
+            "role": "user",
+            "content": "¿Qué cantidad de hierro se recomienda para un cultivo de limón tahití en su cuarto año?"
+          },
+          {
+            "role": "assistant",
+            "content": "ara un cultivo de limón tahití en su cuarto año, se recomienda aplicar 4 kilos de hierro por hectárea."
+          },
+          {
+            "role": "user",
+            "content": "¿Cuánto cobre se debe aplicar a un cultivo de limón tahití de 4 a 5 años?"
+          },
+          {
+            "role": "assistant",
+            "content": "Un cultivo de limón tahití de entre 4 y 5 años requiere 4 kilos de cobre por hectárea."
+          },
+          {
+            "role": "user",
+            "content": "¿Qué cantidad de manganeso se necesita para un cultivo de limón tahití de entre 1 y 2 años?"
+          },
+          {
+            "role": "assistant",
+            "content": "Para un cultivo de limón tahití de entre 1 y 2 años, se deben aplicar 8 kilos de manganeso por hectárea."
+          },
+          {
+            "role": "user",
+            "content": "¿Cuánto zinc se recomienda para un cultivo de limón tahití en su segundo año?"
+          },
+          {
+            "role": "assistant",
+            "content": "En el segundo año, un cultivo de limón tahití requiere 3 kilos de zinc por hectárea."
+          },
+          {
+            "role": "user",
+            "content": "¿Qué cantidad de boro se debe suministrar a un cultivo de limón tahití de 2 a 3 años?"
+          },
+          {
+            "role": "assistant",
+            "content": "Para un cultivo de limón tahití de entre 2 y 3 años, se deben aplicar 4 kilos de boro por hectárea."
+          },
+          {
+            "role": "user",
+            "content": "¿Cuánto azufre se requiere para un cultivo de limón tahití en su primer año?"
+          },
+          {
+            "role": "assistant",
+            "content": "En el primer año, un cultivo de limón tahití necesita 3 kilos de azufre por hectárea."
+          },
+        
+        { role: 'user', content: question }
+      ]
     });
 
     res.json({ response: chatCompletion.choices[0].message.content });
@@ -46,8 +130,7 @@ app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
 
-// Registrar todas las solicitudes
 app.use((req, res, next) => {
   console.log(`Solicitud recibida: ${req.method} ${req.path}`);
   next();
-}); 
+});

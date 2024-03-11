2db1933a5c6eab16d1bf848a9877feded8a25f21
LuisaEstradaY <yepesluu@gmail.com>
2024-03-11 13:49:02 -0500

    mejora

--- preguntaRespuesta.js
+++ preguntaRespuesta.js
@@ -84,6 +84,10 @@ app.post("/find-and-store-link", async (req, res) => {
 
 
 app.post("/get-response", async (req, res) => {
+  const contexts = [
+    { content: " ¿Cómo se siembra un árbol de limón Tahití?", answer: " Debe realizarse con el inicio de las lluvias, aunque la disponibilidad de riego permitirá realizar esta labor en cualquier época del año. Una vez ubicadas las plantas en los sitios de plantación, se retira la bolsa y se ubica la planta en el centro del hoyo (de 40x40x40 cm, estas dimensiones pueden variar en relación con las características del suelo), procurando que el cuello quede unos 5-10 cm por encima de la superficie. Otro tipo de metodología es realizar siembra en \"tortas\". Esto consiste en armar un montículo de tierra de unos 30 o 40 cm de altura y sembrar el árbol en el medio de él. Esto hará que el árbol al expandir las raíces se encuentre con tierra suelta y pueda captar más agua y más nutrientes y sin mayor esfuerzo. A diferencia de la siembra en hoyo no se encontrará con capas duras en el suelo en sus primeras etapas que retrasen o detengan su crecimiento. En ambos casos el diámetro del plato debe de ser de 3 metros, aplicar un pre emergente para prevenir las arvenses y el árbol debe de ir acompañado de un tutor. Refuerza tus conocimientos, ¡visualiza este video complementario ahora!", link: "https://ejemplo.com/reset-password" },
+
+  ];
   try {
     const question = req.body.question;
     if (typeof question !== "string" || question.trim().length === 0) {
